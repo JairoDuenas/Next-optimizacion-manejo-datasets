f1479472a61e4e39015bde27b7e19f883c6e8533
JairoDuenas <jairoduenas.ing@gmail.com>
2022-06-13 15:34:20 -0500

    Autenticación GitHub y local

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,17 +4,21 @@ import { useServerStyles } from '@ui/ssr'
 import { UIProvider } from '@ui/Provider'
 import { QueryProvider } from '@api/QueryProvider'
 
+import { Provider as AuthProvider } from 'next-auth/client'
+
 import '../ui/globals.css'
 
 const NextApp = ({ Component, pageProps }: AppProps) => {
   useServerStyles()
 
   return (
+    <AuthProvider session={pageProps.session}>
     <QueryProvider>
       <UIProvider>
         <Component {...pageProps} />
       </UIProvider>
     </QueryProvider>
+  </AuthProvider>
   )
 }
 
